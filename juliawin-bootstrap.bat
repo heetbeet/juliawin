c2e96f6da4ad2439b5c8b1a1275a02d8b2483572
simon <example@example.com>
2020-10-18 18:21:55 +0200

    Add a bootstrapper to get everything going.

--- juliawin-bootstrap.bat
+++ juliawin-bootstrap.bat
@@ -1,1 +1,3 @@
-powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/refactor/src/julia-win-installer.bat','%tmp%\jl.bat'); (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/refactor/src/functions.bat','%tmp%\functions.bat'); "%tmp%\jl.bat" /P"
+powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/refactor/src/julia-win-installer.bat', '%tmp%\julia-win-installer.bat')"
+powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/refactor/src/functions.bat', '%tmp%\functions.bat')"
+"%tmp%\julia-win-installer.bat"
