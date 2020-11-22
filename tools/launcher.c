a444176b643f3874887c115ffccc7028cdaa1d01
simon <none@none.com>
2020-11-22 20:32:58 +0200

    Update launcher to be the same as my other projects.

--- tools/launcher.c
+++ tools/launcher.c
@@ -17,6 +17,9 @@ int main( int argc, char ** argv )
     TCHAR* cmdArgs = GetCommandLineW();
     TCHAR* cmdPath;
     cmdPath = (TCHAR*) malloc((_tcslen(cmdArgs)+1)*2);
+    cmdPath[0] = '\0';
+    cmdPath[1] = '\0';
+
     _tcscpy(cmdPath, cmdArgs);
 
 
@@ -66,6 +69,9 @@ int main( int argc, char ** argv )
     //*******************************************
     TCHAR* cmdBaseDir;
     cmdBaseDir = (TCHAR*) malloc((_tcslen(cmdPath)+1)*2);
+    cmdBaseDir[0] = '\0';
+    cmdBaseDir[1] = '\0';
+
     _tcscpy(cmdBaseDir, cmdPath);
 
 
@@ -93,18 +99,22 @@ int main( int argc, char ** argv )
     //*******************************************
     TCHAR* cmdName;
     cmdName = (TCHAR*) malloc((_tcslen(cmdPath)+1)*2);
+    cmdName[0] = '\0';
+    cmdName[1] = '\0';
+
     _tcscpy(cmdName, cmdPath);
 
     cmdName = &cmdPath[slashLoc==0?0:slashLoc*2+2];
     int fnameend = _tcslen(cmdName);
-
+    
     // if we run as path\program.exe then we need to truncate the .exe part
     if(0 < fnameend-4 && cmdName[(fnameend-4)*2] == '.'){
         cmdName[(fnameend-4)*2]   = '\0';
         cmdName[(fnameend-4)*2+1] = '\0';
     }
 
-    //_tprintf(L"%s\n", cmdName);
+    //_tprintf(cmdName);
+    //_tprintf(L"\n");
 
     //********************************************
     //Bat name to be checked
@@ -112,49 +122,86 @@ int main( int argc, char ** argv )
     int totlen;
 
     TCHAR* batFile1  = cmdBaseDir;
-    TCHAR* batFile2  = L"\\bin\\";
-    TCHAR* batFile3  = cmdName;
-    TCHAR* batFile4  = L".bat";
+    TCHAR* batFile2  = L"\\bin\\";     //First look in bin
+    TCHAR* batFile3  = L"\\scripts\\"; //then in scripts
+    TCHAR* batFile4  = L"\\src\\";     //then in src
+    TCHAR* batFile5  = L"\\";          //then in same dir
+    TCHAR* batFile6  = cmdName;
+    TCHAR* batFile7  = L".bat";        //Try bat, cmd, vbs, ps1
+    TCHAR* batFile8  = L".cmd";
+    TCHAR* batFile9  = L".vbs";
+    TCHAR* batFile10 = L".ps1";
 
-    totlen = (_tcslen(batFile1)+ _tcslen(batFile2)+ _tcslen(batFile3)+ _tcslen(batFile4));
+    totlen = (_tcslen(batFile1)+_tcslen(batFile2)+_tcslen(batFile3)+_tcslen(batFile4)+_tcslen(batFile5)+_tcslen(batFile6)+_tcslen(batFile7)+_tcslen(batFile8)+_tcslen(batFile9)+_tcslen(batFile10));
 
     TCHAR* batFile;
     batFile = (TCHAR*) malloc((totlen+1)*2);
-    _tcscpy(batFile, batFile1);
-    _tcscat(batFile, batFile2);
-    _tcscat(batFile, batFile3);
-    _tcscat(batFile, batFile4);
+    batFile[0] = '\0';
+    batFile[1] = '\0';
 
-    if(0 != _waccess(batFile, 0)){
-        system("powershell -command \"[reflection.assembly]::LoadWithPartialName('System.Windows.Forms')|out-null;[windows.forms.messagebox]::Show('Could not find the launcher .bat in bin directory.', 'Execution error')\" ");
-    };
+    bool is_powershell = false;
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            _tcscpy(batFile, batFile1);
+            if     (i==0){_tcscat(batFile, batFile2);}
+            else if(i==1){_tcscat(batFile, batFile3);}
+            else if(i==2){_tcscat(batFile, batFile4);}
+            else if(i==3){_tcscat(batFile, batFile5);}
+            _tcscat(batFile, batFile6);
+            if     (j==0){_tcscat(batFile, batFile7);}
+            else if(j==1){_tcscat(batFile, batFile8);}
+            else if(j==2){_tcscat(batFile, batFile9);}
+            else if(j==3){_tcscat(batFile, batFile10);}
+        
+            //test if c:\path\to\cmdName.ext exists
+            if(0 == _waccess(batFile, 0)){
+                if(j==3){
+                    is_powershell = true;
+                }
+                goto breakout;
+            }
+        }
+    }
+    system("powershell -command \"[reflection.assembly]::LoadWithPartialName('System.Windows.Forms')|out-null;[windows.forms.messagebox]::Show('Could not find the launcher .bat in bin directory.', 'Execution error')\" ");
+    breakout:;
 
-    //_tprintf(L"%s\n", batFile);
+    //_tprintf(batFile);
+    //_tprintf(L"\n");
 
     //*******************************************
     //Get into this form: cmd.exe /c ""c:\path\...bat" arg1 arg2 ... "
     //*******************************************
-    TCHAR* cmdLine1  = L"cmd.exe /c \"";
-    TCHAR* cmdLine2  = L"\"";
-    TCHAR* cmdLine3  = batFile;
-    TCHAR* cmdLine4  = L"\" "; 
-    TCHAR* cmdLine5  = cmdArgs;
-    TCHAR* cmdLine6 = L"\"";
+    TCHAR* cmdLine1  = L"powershell.exe -executionpolicy bypass ";
+    TCHAR* cmdLine2  = L"cmd.exe /c \"";
+    TCHAR* cmdLine3  = L"\"";
+    TCHAR* cmdLine4  = batFile;
+    TCHAR* cmdLine5  = L"\" "; 
+    TCHAR* cmdLine6  = cmdArgs;
+    TCHAR* cmdLine7 = L"\"";
 
-    totlen = (_tcslen(cmdLine1)+_tcslen(cmdLine2)+_tcslen(cmdLine3)+_tcslen(cmdLine4)+_tcslen(cmdLine5)+_tcslen(cmdLine6));
+    totlen = (_tcslen(cmdLine1)+_tcslen(cmdLine2)+_tcslen(cmdLine3)+_tcslen(cmdLine4)+_tcslen(cmdLine5)+_tcslen(cmdLine6)+_tcslen(cmdLine7));
 
     TCHAR* cmdLine;
     cmdLine = (TCHAR*) malloc((totlen+1)*2);
+    cmdLine[0] = '\0';
+    cmdLine[1] = '\0';
 
     //Pick vba sequence
-    _tcscpy(cmdLine, cmdLine1);
-    _tcscat(cmdLine, cmdLine2);
+    if(is_powershell){
+        _tcscpy(cmdLine, cmdLine1);
+    }else{
+        _tcscat(cmdLine, cmdLine2);
+    }
     _tcscat(cmdLine, cmdLine3);
     _tcscat(cmdLine, cmdLine4);
     _tcscat(cmdLine, cmdLine5);
     _tcscat(cmdLine, cmdLine6);
-
-    //_tprintf(L"%s\n", cmdLine);
+    if(!is_powershell){
+        _tcscat(cmdLine, cmdLine7);
+    }
+    
+    //_tprintf(cmdLine);
+    //_tprintf(L"\n");
 
     //************************************
     //Prepare and run CreateProcessW
