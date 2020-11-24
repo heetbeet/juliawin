172e8ec8a4e7a24236f8e790897265c7bb767a27
heetbeet <sfstreicher@gmail.com>
2020-11-24 03:45:48 +0200

    Update juliawin-bootstrap.bat

--- juliawin-bootstrap.bat
+++ juliawin-bootstrap.bat
@@ -2,6 +2,7 @@
 mkdir "%temp%\juliawin\src" > nul 2>&1
 mkdir "%temp%\juliawin\tools" > nul 2>&1
 powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/master/src/julia-win-installer.bat', '%temp%\juliawin\src\julia-win-installer.bat')"
+powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/master/src/julia-win-installer.jl', '%temp%\juliawin\src\julia-win-installer.jl')"
 powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/master/src/functions.bat', '%temp%\juliawin\src\functions.bat')"
 powershell.exe -c "(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/heetbeet/juliawin/master/tools/unix2dos.exe', '%temp%\juliawin\tools\unix2dos.exe')"
 
