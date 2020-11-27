e5a26ee6ea781f19f12c20f9f514c291c8d2bd4c
heetbeet <sfstreicher@gmail.com>
2020-11-27 20:03:53 +0200

    Update juliawin_cli.jl

--- internals/juliawin_cli.jl
+++ internals/juliawin_cli.jl
@@ -1,20 +1,13 @@
 using Pkg
 include(joinpath(@__DIR__, "routines.jl"))
 
-function tryusing(pkgsym)
-  try
-    @eval using $pkgsym
-    return true
-  catch e
-      return e
-  end
-  
-if tryusing(:ArgParse) !== true
-  Pkg.add("ArgParse")
-  using ArgParse
-end
+add_startup_script()
+using Pkg.add("Revise")
+using Pkg.add("OhMyREPL")
+using Pkg.add("ArgParse")
 
-  
+using ArgParse
+
 s = ArgParseSettings(description = "This is the main commnadline interface to Juliawin.")
 @add_arg_table s begin
     "--install"
@@ -26,3 +19,51 @@ end
   
 parsed_args = parse_args(ARGS, s)
   
+
+if parsed_args["install"] !== nothing
+    println("This is not implemented yet")
+  
+elseif parsed_args["install-dialog"]
+  
+    vscodeinstall = nothing
+    while !(vscodeinstall in ("y", "n") )
+      vscodeinstall = lowercase(Input("Install VSCode [Y/N]? "))
+    end 
+  
+    junoinstall = nothing
+    while !(junoinstall in ("y", "n") )
+      junoinstall = lowercase(Input("Install Juno [Y/N]? "))
+    end 
+  
+    plutoinstall = nothing
+    while !(plutoinstall in ("y", "n") )
+      plutoinstall = lowercase(Input("Install Pluto [Y/N]? "))
+    end 
+  
+    jupyterinstall = nothing
+    while !(jupyterinstall in ("y", "n") )
+      jupyterinstall = lowercase(Input("Install Python/Conda and Jupyter [Y/N]? "))
+    end
+  
+  
+    if !isfile("$juliawinpackages/curl/bun/curl.exe")
+        install_curl()
+    end
+  
+    if vscodeinstall == "y"
+        install_vscode()
+    end
+   
+    if junoinstall == "y"
+        install_atom()
+        install_juno()
+    end
+  
+    if plutoinstall == "y"
+        install_pluto()
+    end
+    
+    if jupyterinstall == "y"
+       install_jupyter()
+    end
+end
