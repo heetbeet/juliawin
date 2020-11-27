068f9feb9aebbc544a5bc52b8176e62443e5ecb8
heetbeet <sfstreicher@gmail.com>
2020-11-27 19:35:39 +0200

    Update juliawin_cli.jl

--- internals/juliawin_cli.jl
+++ internals/juliawin_cli.jl
@@ -1,4 +1,5 @@
 using Pkg
+include(joinpath(@__DIR__, "routines.jl"))
 
 function tryusing(pkgsym)
   try
@@ -12,3 +13,16 @@ if tryusing(:ArgParse) !== true
   Pkg.add("ArgParse")
   using ArgParse
 end
+
+  
+s = ArgParseSettings(description = "This is the main commnadline interface to Juliawin.")
+@add_arg_table s begin
+    "--install"
+        help = "Choose specifig package to install"
+    "--install-dialog", "-d"
+        help = "Enter the installation guide."
+        action = :store_true
+end
+  
+parsed_args = parse_args(ARGS, s)
+  
