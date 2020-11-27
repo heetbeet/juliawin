70ca0327197202416ea6383f4ea55f4466b79eaa
simon <none@none.com>
2020-11-27 13:38:40 +0200

    Added super github bootstrapper that magically pulls the rest of the installer our of thin air. Also added a bootstrapper to continue the rest of the installation from the current directory.

---
+++ bin/bootstrap-juliawin-from-github.bat
@@ -0,0 +1,85 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: ***************************************
+:: This is the start of our juliawin bootstrapping
+:: Unfortunately we may not use any external function yet
+:: We may also not use any gotos yet:
+:: https://serverfault.com/questions/429594/is-it-safe-to-write-batch-files-with-unix-line-endings
+:: ***************************************
+
+
+:: ***************************************
+:: Download the master zip directly from github
+:: ***************************************
+:: This is the most general legacy powershell download command. It should be available on any powershell
+echo () Download juliawin from github to temp
+set "juliawinzip=%temp%\juliawin-%random%%random%.zip"
+call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/heetbeet/juliawin2/archive/main.zip', '%juliawinzip%')"
+
+
+:: ***************************************
+:: Unzip the master zip into a temporary directory
+:: ***************************************
+:: https://stackoverflow.com/questions/21704041/creating-batch-script-to-unzip-a-file-without-additional-zip-tools
+echo () Unzip juliawin to temp
+set "juliawintemp=%temp%\juliawin-%random%%random%"
+mkdir "%juliawintemp%" 2>NUL
+
+set "vbs=%temp%\_%random%%random%.vbs"
+
+set vbs_quoted="%vbs%"
+> "%vbs%"  echo set objShell = CreateObject("Shell.Application")
+>>"%vbs%"  echo set FilesInZip=objShell.NameSpace("%juliawinzip%").items
+>>"%vbs%"  echo objShell.NameSpace("%juliawintemp%").CopyHere(FilesInZip)
+
+cscript //nologo "%vbs%"
+del "%vbs%" /f /q > nul 2>&1
+
+
+:: ***************************************
+:: Copy everything to the final location
+:: ***************************************
+set "install-directory=%userprofile%\Juliawin"
+set "vbs=%temp%\_%random%%random%.vbs"
+set "bat=%vbs%.bat"
+
+:: Wow, this is so difficult without a goto...
+for %%a in (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1) do (
+    if /i "!defaultinstall!" neq "Y" if /i "!defaultinstall!" neq "N" (
+        set /P defaultinstall="Install into default location %install-directory% [Y/N]? "
+    )
+)
+> "%vbs%" echo set shell=WScript.CreateObject("Shell.Application")
+>>"%vbs%" echo set f=shell.BrowseForFolder(0,"Select Juliwin install directory",0,"")
+>>"%vbs%" echo if typename(f)="Nothing" Then
+>>"%vbs%" echo    wscript.echo "set __returnval__="
+>>"%vbs%" echo    WScript.Quit(1)
+>>"%vbs%" echo end if
+>>"%vbs%" echo set fs=f.Items():set fi=fs.Item()
+>>"%vbs%" echo p=fi.Path:wscript.echo "set __returnval__=" ^& p
+
+if /i "%defaultinstall%" NEQ "Y" (
+    call cscript //nologo "%vbs%" > "%bat%" && call "%bat%"
+)
+del "%vbs%" /f /q > nul 2>&1
+del "%bat%" /f /q > nul 2>&1
+
+if /i "%defaultinstall%" NEQ "Y" (
+    if "%__returnval__%" equ "" (
+        echo ^(^) Invalid or no directory provided, please restart installer.
+        pause
+        exit /b -1
+    ) else (
+        set "install-directory=%__returnval__%"
+    )
+)
+
+robocopy "%juliawintemp%\juliawin2-main" "%install-directory%" /s /e /mov
+del "%juliawinzip%" /f /q > nul 2>&1
+
+
+:: ***************************************
+:: Run the newly aquired local version
+:: ***************************************
+call "%install-directory%\bootstrap-juliawin-from-local-directory.bat"
---
+++ bin/bootstrap-juliawin-from-local-directory.bat
@@ -0,0 +1,55 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: Access to external functions
+set functions="%~dp0functions.bat"
+
+call %functions% ARG-PARSER %*
+if "%ARG_h%%ARG_help%" NEQ "" (
+    goto :PRINT-HELP
+)
+
+:: Test if we should forcefully install julia
+if exist "%dp0..\packages\julia\bin\julia.exe" if "%ARG_force%" neq "1" (
+    call :PROMPT-FORCEINSTALL forceinstall
+)
+if /i "%forceinstall%" EQU "N" exit /b -1
+
+
+:: Install Julia
+call %functions% DELETE-DIRECTORY "%~dp0..\packages\julia" 2 > nul
+call "%~dp0\bootstrap-julia-from-julialang-org.bat" "%~dp0..\packages\julia"
+
+
+:: Run juliawin installation script
+call "%~dp0\juliawin.bat" --install
+
+
+goto :eof
+
+::***************************
+:: Print the help menu
+::***************************
+:PRINT-HELP
+    echo Script to install Juliawin into a specified directory.
+    echo:
+    echo Usage:
+    echo   %~n0 [options]
+    echo Options:
+    echo   /h, /help      Print these options
+    echo   /force         Overwrite current "/packages/julia" installation without prompt
+goto :eof
+
+
+::**************************
+:: Prompt forceinstall
+::**************************
+:PROMPT-FORCEINSTALL <answer>
+    set "%~1="
+    for %%a in (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1) do (
+        if /i "!forceinstall!" neq "Y"  if /i "!forceinstall!" neq "N" (
+            set /P forceinstall="Julia installation in packages/julia exist, overwrite [Y/N]? "
+        )
+    )
+    set "%~1=%forceinstall%"
+goto :eof
