d528f1fb5c93c6ebc5c5eee95fc32a49fa4389e8
heetbeet <sfstreicher@gmail.com>
2020-11-30 11:35:23 +0200

    Update juliawinconfig.jl
    Force libgit2 to download packages in stead of downloading tarballs

--- userdata/.julia/config/juliawinconfig.jl
+++ userdata/.julia/config/juliawinconfig.jl
@@ -1,9 +1,9 @@
-# Juliawin uses curl as the default downloader
+# Force libgit2 to download packages in stead of downloading tarballs
+ENV["JULIA_PKG_SERVER"] = ""
 
+# Juliawin uses curl as the default downloader
 if Sys.iswindows()
-    #*****************************
     # Add curl from packages to path
-    #*****************************
     for i=1 #to keep scope clear
         packagedir = abspath(String(@__DIR__)*raw"/../../../packages")
         curlpackages = [i for i in readdir(packagedir) if startswith(i, "curl")]
@@ -12,10 +12,7 @@ if Sys.iswindows()
         end
     end
 
-
-    #*****************************
     # Make use of curl and overwrite download_powershell for stubborn libraries.
-    #*****************************
     if Sys.which("curl") !== nothing
         ENV["BINARYPROVIDER_DOWNLOAD_ENGINE"] = "curl"
         Base.download_powershell(url::AbstractString, filename::AbstractString) = Base.download_curl(Sys.which("curl"), url, filename)
